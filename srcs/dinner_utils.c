f9bc2be2b2c96b30dc8903798b201b5751adf9bf
mariocos <alpacasrcool7@gmail.com>
2024-11-21 16:58:32 +0000

    changed the sleep in eat to the right place

--- srcs/dinner_utils.c
+++ srcs/dinner_utils.c
@@ -12,15 +12,15 @@ void	eat(t_philo *p)
 	safe_mutex_handle(&p->scnd_fork->fork_mtx, LOCK);
 	print_status(TAKE_SECOND_FORK, p);
 
-
 	set_long(&p->p_mtx, &p->last_meal_time, get_time(MILISECOND));
 
 	increment_long(&p->p_mtx, &p->n_meals);
 
 	print_status(EATING, p);
+
+	good_sleep(get_long(&p->table->table_mtx, &p->table->data->time_to_sleep), p->table);
 	if (p->table->data->max_meals > 0 && p->n_meals == p->table->data->max_meals)
 		set_bool(&p->p_mtx, &p->is_full, true);
 	safe_mutex_handle(&p->first_fork->fork_mtx, UNLOCK);
 	safe_mutex_handle(&p->scnd_fork->fork_mtx, UNLOCK);
-	good_sleep(get_long(&p->table->table_mtx, &p->table->data->time_to_sleep), p->table);
 }
